6bb714e4725d2b24049ebd6e394ff13c85ca04a6
JDesign <joel.jdesignera@gmail.com>
2021-05-10 05:05:05 +0800

    Add about command

---
+++ commands/about.js
@@ -0,0 +1,31 @@
+import { MessageEmbed } from "discord.js";
+
+export const name = "about";
+export function execute(client, message, args) {
+  const embed = new MessageEmbed()
+    .setColor("#2576A3")
+    .setTitle("サメです、here is more info about creator!")
+    .setAuthor("JDεꜱɪɢɴ",
+      "https://www.dropbox.com/s/88t69bfojsw4df1/avatar.png?raw=1",
+      "https://jdesignera.com/"
+    )
+    .setDescription(`
+      
+      Here are some links where you can learn more about him:
+    `.replace(/  +/g, ''))
+    .addFields(
+      {
+        name: "Platform",
+        value: `
+          [Website](https://jdesignera.com/)
+          [Discord](https://discord.com/users/156834654140235776)
+          [GitHub](https://github.com/jdesignera)
+          [GPG](https://jdesignera.com/JDesign_0xA313E5EF_public.asc)
+
+        `.replace(/  +/g, '')
+      }
+    )
+    .setFooter("</> with <3 by JDεꜱɪɢɴ™#1111");
+
+    message.channel.send(embed);
+}
--- commands/help.js
+++ commands/help.js
@@ -3,13 +3,14 @@ import { MessageEmbed } from "discord.js";
 export const name = "help";
 export function execute(client, message, args) {
   const embed = new MessageEmbed()
-    .setColor(process.env.MAIN_COLOR)
+    .setColor("#2576A3")
     .setTitle("サメです、I am here to help!")
     .addFields(
       {
         name: "Command",
         value: `
-          \`hello\`,
+          \`about\`
+          \`hello\`
           \`insult <@user>\`,
           \`clear <all / int>\`
         `.replace(/  +/g, ''),
@@ -18,6 +19,7 @@ export function execute(client, message, args) {
       {
         name: "Description",
         value: `
+          Info about my creator.
           I shall greet you.
           I shall insult someone for you.
           Clear messages in that channel. **(Admin)**
