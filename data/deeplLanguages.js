72692e611e02a4b6ee930559f7c4659382563936
JDesign <joel.jdesignera@gmail.com>
2021-05-21 19:33:28 +0800

    Add translate command

---
+++ data/deeplLanguages.js
@@ -0,0 +1,16 @@
+export default {
+  "de": "German",
+  "el": "Greek",
+  "en": "English",
+  "es": "Spanish",
+  "fr": "French",
+  "it": "Italian",
+  "ja": "Japanese",
+  "nl": "Dutch",
+  "pl": "Polish",
+  "pt": "Portuguese",
+  "ro": "Romanian",
+  "ru": "Russian",
+  "sb": "Swedish",
+  "zh": "Chinese"
+}
