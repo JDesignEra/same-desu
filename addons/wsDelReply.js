0a09c345edad4287109452069e85064e375b9e22
JDesign <joel.jdesignera@gmail.com>
2021-06-05 04:39:35 +0800

    Update insult slash command respond method

--- addons/wsDelReply.js
+++ addons/wsDelReply.js
@@ -1,3 +1,4 @@
 export default async (client, interaction) => {
-  client.api.interactions(interaction.id, interaction.token).callback.delete();
+  client.api.webhooks(client.user.id, interaction.token).messages('@original').delete();
+  // client.api.interactions(interaction.id, interaction.token).callback.delete();
 }
