d5a7a9e3dc28c3201cb47c70f0d79637952158ef
JDesign <joel.jdesignera@gmail.com>
2021-06-06 02:35:29 +0800

    Insult commands insult more in sync audio to text

--- data/insults.js
+++ data/insults.js
@@ -1,97 +1,97 @@
 export default [
   {
     insult: "Orae! Bitch! FAQ <user>!",
-    attachment: "Miko/bitch_faq.mp3",
+    attachment: "Miko/Bitch! FAQ!.mp3",
     attachmentType: "audio",
     state: true
   },
   {
     insult: "You're die <user>!",
-    attachment: "Miko/you_are_die.mp3",
+    attachment: "Miko/You're die.mp3",
     attachmentType: "audio",
     state: true
   },
   {
     insult: "ハアアア、うるせえ <user>！",
-    attachment: "Miko/haaah_usae.mp3",
+    attachment: "Miko/Haaah, noisy.mp3",
     attachmentType: "audio",
     state: true
   },
   {
     insult: "English motherfucker, do you speak it <user>!?",
-    attachment: "Miko/english_motherfucker.wav",
+    attachment: "Miko/English motherfucker, do you speak it.wav",
     attachmentType: "audio",
     state: true
   },
   {
     insult: "What the faq <user>!?",
-    attachment: "Miko/what_the_faq.mp3",
+    attachment: "Miko/What the FAQ.mp3",
     attachmentType: "audio",
     state: true
   },
   {
     insult: "FAQQQQQ <user>!",
-    attachment: "Miko/faqqqqqq.mp3",
+    attachment: "Miko/Faqqqqqq.mp3",
     attachmentType: "audio",
     state: true
   },
   {
     insult: "Kiss my ass <user>!",
-    attachment: "Miko/kiss_my_ass.mp3",
+    attachment: "Miko/Kiss my ass.mp3",
     attachmentType: "audio",
     state: true
   },
   {
     insult: "Stay home だよ <user>!",
-    attachment: "Miko/stay_home_dayo.wav",
+    attachment: "Miko/Stay home dayo.wav",
     attachmentType: "audio",
     state: true
   },
   {
-    insult: "<user> ばか、ばか、ばか、ウンチ、ウンチ、ウンチ。",
-    attachment: "Miko/baka_baka_baka_poop_poop_poop.mp3",
+    insult: "<user> ばかばか、ウンチ、ウンチ、ウンチ、ウンチ。",
+    attachment: "Miko/Idiot, idiot, poop, poop, poop, poop.mp3",
     attachmentType: "audio",
     state: true
   },
   {
     insult: "<user> ばかーばかーばかーばかーばかだねぇ、ぷぷぷ！",
-    attachment: "Miko/baka_baka_pupu.mp3",
+    attachment: "Miko/Idiot, idiot, idiot, idiot, idiot, pfft!.mp3",
     attachmentType: "audio",
     state: true
   },
   {
-    insult: "ばか！おおおうう、ばか、ばか <user>！",
-    attachment: "Pekora/baka_oh_baka_baka.wav",
+    insult: "ばか！おおおおおーうう、ばか、ばか <user>。",
+    attachment: "Pekora/Idiot! Ohhhhhhh, idiot, idiot.wav",
     attachmentType: "audio",
     state: true
   },
   {
     insult: "くそガキがよ、早くご飯食いに行けよ、<user> 馬鹿野郎！",
-    attachment: "Pekora/damn_you_kid_get_out_now_and_go_dinner,_you_idiot.mp3",
+    attachment: "Pekora/You little shit, hurry up and go eat your dinner now, you idiot!.mp3",
     attachmentType: "audio",
     state: true
   },
   {
     insult: "<user> 見てんじゃねよこの放送をよ！くそガキが！",
-    attachment: "Pekora/don_you_fucking_watch_this_fucking_broadcast_you_little_shit.mp3",
+    attachment: "Pekora/Don't you fucking watch this fucking broadcast! You little shit!.mp3",
     attachmentType: "audio",
     state: true
   },
   {
     insult: "早く正月あけれ… <user> 明けて学校に行ってみろよ！",
-    attachment: "Pekora/finish_your_winter_vacation_and_go_back_to_grade_school.mp3",
+    attachment: "Pekora/Hurry up and finish up your new your and get back to your school.mp3",
     attachmentType: "audio",
     state: true
   },
   {
-    insult: "<user> おまえ、体育館の裏に来いよ。ボコボコにしてやっからな。",
-    attachment: "Pekora/come_to_gym_afterward_i_am_going_to_kick_your_ass.mp3",
+    insult: "<user> おまえあとで、体育館の裏に来いよ。ボコボコにしてやっからな。",
+    attachment: "Pekora/You come to the back of the gym later. I'm gonna beat the shit out of you.mp3",
     attachmentType: "audio",
     state: true
   },
   {
     insult: "You're too small <user>.",
-    attachment: "Gura/yeah_well_your_is_too_small.mp3",
+    attachment: "Gura/You're too small.mp3",
     attachmentType: "audio",
     state: true
   }
